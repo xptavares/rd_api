03fdf29e07d8b1ae906ac316f5337f89aacaa751
Alexandre Tavares <xptavares@gmail.com>
2014-09-04 22:02:06 -0300

    create destroy and update

--- lib/rd_api/account.rb
+++ lib/rd_api/account.rb
@@ -4,25 +4,30 @@ module RdApi
 
 	class Account
 		
-		attr_accessor :id, :name
+		attr_accessor :client
 
-		def initialize(id, name)
-			@id = id
-			@name = name
+		def initialize
+			@client = Restforce.new
 		end
 
 		def all
 
 		end
 
-		def find_by_id 
-			client = Restforce.new 
-			client.find('Account', @id)
+		def find_by_id(id)
+			client.find('Account', id)
 		end
 
 		def create(name)
-			client = Restforce.new
 			client.create('Account', Name: name)
+		end
+
+		def update(id, name)
+			client.update('Account', Id: id, Name: name)
+		end
+
+		def destroy(id)
+			client.destroy('Account', id)
 		end
 	end
 
--- spec/rd_api_spec.rb
+++ spec/rd_api_spec.rb
@@ -5,17 +5,30 @@ describe RdApi::Account do
 	let(:id) { '001o0000007gkAOAAY' }
 	let(:name) { 'GenePoint' }
 	let(:new_name) { 'New Name' }
-	let(:account) { RdApi::Account.new(id, name) }
-	let(:account_name) { account.find_by_id.Name }
-	let(:new_account_id) { account.create new_name }
+	let(:new_edit_name) { 'New Edit Name' }
+	let(:account) { RdApi::Account.new }
+	let(:account_name) { account.find_by_id(id).Name }
+	let(:new_account_id) { account.create(new_name) }
 
   	it '#account_name' do
   		expect(account_name).to eq name
   	end
 
-  	it '#create_account' do
-  		account.id = new_account_id
-  		expect(account.find_by_id.Name).to eq new_name
+  	it '#create' do
+  		expect(account.find_by_id(new_account_id).Name).to eq new_name
+  	end
+
+  	it '#update' do
+  		expect(account.update(new_account_id, new_edit_name)).to be_truthy
+  	end
+
+  	it '#update, compere edit names' do
+  		account.update(new_account_id, new_edit_name)
+  		expect(account.find_by_id(new_account_id).Name).to eq new_edit_name
+  	end
+
+  	it '#destroy' do
+  		expect(account.destroy(new_account_id)).to be_truthy
   	end
   	
 end
